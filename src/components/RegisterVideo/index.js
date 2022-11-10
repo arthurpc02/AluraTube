5a687c5f2d38d5404558db2116230bb16279508c
arthur <arthurpc02@gmail.com>
2022-11-10 12:21:16 -0300

    criou form e botao para colocar e tirar o form.

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,13 +1,35 @@
+import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
 export default function RegisterVideo() {
-    // falta adicionar o floating button
-    // escurecer o fundo quando abre o register video (modal)
-    // -> precisamos controlar o state
+    // [x] falta adicionar o floating button
+    // [x] escurecer o fundo quando abre o register video (modal)
+    // -> [x]  precisamos controlar o state
     // -> formulário do register video
+
+    const [formVisivel, setFormVisivel] = React.useState(false);
+
     return (
         <StyledRegisterVideo>
-            {/* Chamada do RegisterVideo */}
+            <button className="add-video" onClick={() => setFormVisivel(true)}>
+                +
+            </button>
+            {formVisivel
+                ? (
+                    <form>
+                        <div>
+                            <button className="close-modal" onClick={() => setFormVisivel(false)}>
+                                X
+                            </button>
+                            <input placeholder="Título do vídeo" />
+                            <input placeholder="URL" />
+                            <button type="submit">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>
+                )
+                : false}
         </StyledRegisterVideo>
     )
 }
